5ed978e2dde5155356392a12b6716ef38e1a4c65
unknown <gail.chugg@bigpond.com>
2020-11-08 22:01:07 +0800

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "4049fc8fc4eb7178bbf564b0a37ad3ca"
+weather_api_key = "My key here"
 
 # Google API Key
-g_key = "AIzaSyCE0VqrEvzTgqZo5m0tBUFb5uGpDTW74ZY"
+g_key = "My key here"
